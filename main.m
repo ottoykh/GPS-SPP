d89c206034e0eed62d8cbfea33fa5acc8b8eb53b
Yu Kai Him Otto <112672032+ottoykh@users.noreply.github.com>
2025-04-29 13:47:02 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
-% Before Run this main funtion code 
+% Before Run this main function code 
 % The navigation and observation file MUST convert into RINEX2.10 format 
-% Conversion code is avaliable in the /tools folder, run the "convert.m" 
+% Conversion code is available in the /tools folder, run the "convert.m" 
 % Also Change the folder path, as well the default is relative pathing 
 
 % Broadcast ephemeris
